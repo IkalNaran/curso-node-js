24afdc55517fcb19ab89698ba171435aa4cc6d8a
Ivan Mauricio Velazquez Trejo <ivanmauriciovt@gmail.com>
2024-06-26 18:08:46 -0600

    Se creo carperta de class-1 y se movieron todos los archivos a la carpeta clase 1

--- 1-mjs/index.mjs
+++
@@ -1,9 +0,0 @@
-//.js -> por defecto utiliza CommonJS
-//.msj -> para utilizar ES Modules
-//.cjs -> para forzas a utilizar CommonJS
-
-import { sum, sub, mult } from "./sum.mjs"
-
-console.log(sum(1, 2))
-console.log(sub(1, 2))
-console.log(mult(1, 2))
